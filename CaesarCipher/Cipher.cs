6047a4ec740cb8e6310bc87f3f5e591a902adf7e
LamaMiners <nico.depisch07@gmail.com>
2025-01-28 19:13:27 +0100

    Added Encrypt Page

---
+++ CaesarCipher/Cipher.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace CaesarCipher;
+
+public class Cipher
+{
+    public string Encrypt(char[] message, int shift)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (char c in message)
+        {
+            // Handle uppercase letters (A-Z)
+            if (char.IsUpper(c))
+            {
+                sb.Append((char)((((c - 'A') + shift) % 26 + 26) % 26 + 'A'));
+            }
+            // Handle lowercase letters (a-z)
+            else if (char.IsLower(c))
+            {
+                sb.Append((char)((((c - 'a') + shift) % 26 + 26) % 26 + 'a'));
+            }
+            // For non-alphabetic characters, don't change them
+            else
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
