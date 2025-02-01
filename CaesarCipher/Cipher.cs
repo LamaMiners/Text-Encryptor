7d2e14834af30436b52c835df708fce53ada379a
LamaMiners <nico.depisch07@gmail.com>
2025-02-01 15:05:16 +0100

    Added Decryption

--- CaesarCipher/Cipher.cs
+++ CaesarCipher/Cipher.cs
@@ -29,4 +29,30 @@ public class Cipher
 
         return sb.ToString();
     }
+
+    public string Decrypt(char[] message, int shift)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (char c in message)
+        {
+            // Handle uppercase letters (A-Z)
+            if (char.IsUpper(c))
+            {
+                sb.Append((char)((((c - 'A') - shift) % 26 + 26) % 26 + 'A'));
+            }
+            // Handle lowercase letters (a-z)
+            else if (char.IsLower(c))
+            {
+                sb.Append((char)((((c - 'a') - shift) % 26 + 26) % 26 + 'a'));
+            }
+            // For non-alphabetic characters, don't change them
+            else
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
 }
